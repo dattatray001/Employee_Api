33905bc3505fe91481357601d6acce5ff9df9ce7
dattatray001 <dattatrynarhe@gmail.com>
2024-12-10 09:52:20 +0530

    second commit

--- src/main/java/com/dattta/employeeapi/repository/EmployeeRepository.java
+++ src/main/java/com/dattta/employeeapi/repository/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.dattta.employeeapi.repository;
 
 import com.dattta.employeeapi.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     boolean existsByName(String name);
     boolean existsById(Long id);
